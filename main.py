79ba2b5bbae7be0bc5e6e876bfa717ffa22f127b
ibrahim-59 <116106936+ibrahim-59@users.noreply.github.com>
2023-06-15 18:37:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,66 @@
-from flask import Flask, jsonify
+import json
+from flask import Flask,request,render_template,url_for,jsonify,session
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import joblib as joblib
 import os
+
+model= joblib.load("model_pickle")
+
 
 app = Flask(__name__)
 
+@app.route("/test", methods=['POST','GET'])
+def test():
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    output = request.get_json()
+    d1 = output['d1']
+    d2 = output['d2']
+    d3 = output['d3']
+    d4 = output['d4']
+    d5 = output['d5']
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    disease = []
+    disease.append(int(d1))
+    disease.append(int(d2))
+    disease.append(int(d3))
+    disease.append(int(d4))
+    disease.append(int(d5))
+
+    f = disease[0]
+    b = disease[1]
+    c = disease[2]
+    d = disease[3]
+    e = disease[4]
+    print(disease[4])
+
+    a = []
+    i = 0
+    while i < 134:
+        a.append(0)
+        i += 1
+
+        if i == 134:
+            break
+
+    a[f] = 1
+    a[b] = 1
+    a[c] = 1
+    a[d] = 1
+    a[e] = 1
+    input_data_as_numpy_array = np.asarray(a)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+
+    prediction = model.predict(input_data_reshaped)
+
+    d = dict(enumerate(prediction.flatten(), 1))
+    print(d)
+
+    # this shows the json converted as a python dictionary
+    return jsonify({"result": prediction[0]})
+
+if __name__ == "__main__":
+    app.run(debug=True)
